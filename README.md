1b57701a2fcbd41e2924126e2fa86722c02d90e4
victorsouza19 <victor.cristofalo@hotmail.com>
2021-11-09 23:47:21 -0300

    :tada: first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Hi there :wave:
+
+This is a simple study repository to understand some metrics about form validations and something about good practices of input verifications.
+
+In this study, we used these dependencies for the form validation:
+
+* Express-sessions: It's to create sessions.
+* Express-flash: We use this library to send data into a session, like error messages.
+* Cookie-parser: We need this to parse the data for the cookie on user's browser.
+
+In the future, i plan to analyse the **validator.js** dependency too, for apply in this study.
---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express'),
+app = express(),
+flash = require('express-flash'),
+session = require('express-session'),
+cookieParser = require('cookie-parser');
+
+
+// configure ejs as view engine
+app.set('view engine', 'ejs');
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: false}));
+
+// parse application/json
+app.use(express.json());
+
+// configure cookie parser
+app.use(cookieParser("b7b9686321a86f5508bf47634b72e216"));
+
+// use express-session
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}));
+
+// use express-flash
+app.use(flash());
+
+app.get("/", (req, res) => {
+  let emailErr = req.flash("emailErr");
+  let nameErr = req.flash("nameErr");
+  let pointsErr = req.flash("pointsErr");
+  let email = req.flash("email");
+  let name = req.flash("name");
+  let points = req.flash("points");
+
+  emailErr = (emailErr == undefined || emailErr.length == 0) ? undefined : emailErr;
+  nameErr = (nameErr == undefined || nameErr.length == 0) ? undefined : nameErr;
+  pointsErr = (pointsErr == undefined || pointsErr.length == 0) ? undefined : pointsErr;
+
+  email = (email == undefined || email.length == 0) ? "" : email;
+  points = (points == undefined || points.length == 0) ? "" : points;
+  name = (name == undefined || name.length == 0) ? "" : name;
+
+  res.render("index", {
+    emailErr, nameErr, pointsErr, email, name, points
+  });
+});
+
+app.post("/form", (req, res) => {
+  const { email, name, points } = req.body;
+
+  let emailErr;
+  let nameErr;
+  let pointsErr;
+
+  if(email == undefined || email == ""){
+    emailErr = "E-mail cannot be empty.";
+
+  }
+
+  if(points == undefined || points > 10){
+    pointsErr = "The maximum number of points is 10."
+  }
+
+  if(name == undefined || name == ""){
+    nameErr = "Name cannot be empty.";
+  }
+
+  if(emailErr != undefined || pointsErr != undefined  || nameErr != undefined){
+    req.flash("emailErr", emailErr);
+    req.flash("pointsErr", pointsErr);
+    req.flash("nameErr", nameErr);
+
+    req.flash("email", email);
+    req.flash("name", name);
+    req.flash("points", points);
+
+    res.redirect("/");
+  }else{
+    res.send("It's ok!");
+  }
+});
+
+app.listen(5001, (req, res) => {
+  console.log("Server is running!");
+});
